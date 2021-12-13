defeae6ec0cd01af29a175f1870d1c197909b316
DilaraSeker <dlrseker@gmail.com>
2021-12-13 16:13:58 +0300

    ilk commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>{{count}}</p>
+        <button @click="decrement">DECREMENT</button>
+        <button @click="increment">INCREMENT</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+button{
+  color: #b94283;
+  margin-left: 10px;
+}
+
+</style>
